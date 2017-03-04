ca7d4ea0792991892f3e541c96289f7f69c19ac1
Ilyas Garaev <ilyas.garaev@flatstack.com>
2017-03-04 23:24:28 +0300

    add new specs

--- lib/openzip.rb
+++ lib/openzip.rb
@@ -5,11 +5,17 @@ module Openzip
   extend Fiddle::Importer
 
   def self.lib_ext
-    raise "Sorry, windows is not supported yet" if RUBY_PLATFORM =~ /win32/
+    raise "Sorry, Windows is not supported" if RUBY_PLATFORM =~ /win32/
 
     RUBY_PLATFORM =~ /darwin/ ? "dylib" : "so"
   end
 
   dlload "#{File.dirname(__FILE__)}/../source/target/release/libopenzip.#{lib_ext}"
-  extern "void extract(char*, char*)"
+
+  def self.extract(zippath, outdirpath)
+    extract_rust(zippath, outdirpath) != 0
+  end
+
+  extern "char extract_rust(char*, char*)"
+  private_class_method :extract_rust
 end
